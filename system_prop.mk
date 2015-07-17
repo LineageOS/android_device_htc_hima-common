6663b9a08887930c0d96aa7091fa49104c64e446
Thierry Strudel <tstrudel@google.com>
2015-07-16 18:30:40 -0700

    hima: force dex2oat not to use swap file
    Bug: 20658562
    Change-Id: Ib8e47b3edd94b1977c294959cdde01a54b64f123
    Signed-off-by: Thierry Strudel <tstrudel@google.com>

--- system_prop.mk
+++ system_prop.mk
@@ -114,3 +114,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.qualcomm.sns.daemon=e \
     debug.qualcomm.sns.hal=e \
     debug.qualcomm.sns.libsensor1=e
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-swap=false
+
