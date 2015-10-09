1fd2a1d07cbc182ed57a0b4e9cdccba45e8431db
Rashed Abdel-Tawab <rashed@linux.com>
2015-10-09 18:45:51 -0400

    [DNM] hima: Updates for building M
    Change-Id: I5518442ecbf2877063f92f5294b9a497e4ec8987

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -52,7 +52,7 @@ TARGET_USES_C2D_COMPOSITION := true
 
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := device/htc/hima-common/mkbootimg.mk
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-7 androidusb.pid=0x065d androidkey.dummy=1 androidtouch.htc_event=1 disk_mode_enable=1
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-7 androidusb.pid=0x065d androidkey.dummy=1 androidtouch.htc_event=1 disk_mode_enable=1 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00078000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_RAMDISK_OFFSET := 0x02000000
@@ -155,7 +155,8 @@ BOARD_PROVIDES_ADDITIONAL_BIONIC_STATIC_LIBS += libc_htc_symbols
 TARGET_PROVIDES_LIBLIGHT := true
 
 # Logging
-TARGET_USES_LOGD=false
+#TARGET_USES_LOGD=false
+COMMON_GLOBAL_CFLAGS += -DHTCLOG
 
 # Offmode Charging
 COMMON_GLOBAL_CFLAGS += \
@@ -173,7 +174,7 @@ TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += device/htc/hima-common/sepolicy
+#BOARD_SEPOLICY_DIRS += device/htc/hima-common/sepolicy
 
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
