7d1ac5d224ba0987f910a6b78ca9eca44f545f0b
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-10-24 19:37:44 +1030

    hima: kill deprecated property.
    Does anybody actually still use cwm anymore?
    
    Change-Id: I7354bf5ed9ccd8a374977d681c26b3a14b3f66d2

--- system_prop.mk
+++ system_prop.mk
@@ -6,7 +6,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.io.scheduler=bfq
 
-# MTP and USB-OTG
+# MTP
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
 
@@ -94,10 +94,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.netmgrd.qos.enable=false \
     ro.use_data_netmgrd=true
-
-# Recovery
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.cwm.forbid_format=/boot,/firmware,/persist
 
 # Sensor debugging
 # Valid settings (and presumably what they mean):
