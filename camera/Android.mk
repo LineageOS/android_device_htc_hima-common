07e98354ab9fb2e03ebff2c61b9ff87b599c6622
Ethan Chen <intervigil@gmail.com>
2015-10-29 17:57:44 -0700

    Revert "hima: Remove camera wrapper"
    * Re-add the camera wrapper to disable face detection
    
    This reverts commit 5bba54389d0162a7bb23218286c6ef04ed9b6a02.
    
    Change-Id: Ie86aab2a3bbca424b55166d583901713bf2cb61e

--- camera/Android.mk
+++ camera/Android.mk
@@ -28,3 +28,21 @@ LOCAL_MODULE := libcamera_parameters_ext
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    CameraWrapper.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog libcamera_client libutils
+
+LOCAL_C_INCLUDES += \
+    system/media/camera/include
+
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_TAGS := optional
+LOCAL_32_BIT_ONLY := true
+
+include $(BUILD_SHARED_LIBRARY)
