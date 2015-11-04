b5cf33a075cc0a976da7925bd9f889c6a1061c55
Ethan Chen <intervigil@gmail.com>
2015-11-03 16:06:14 -0800

    hima: WIP: Fix SELinux
    Change-Id: If295a09b8bf113f53e0eee00a1fc0398ccfd9916

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -47,7 +47,7 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_BASE := 0x00078000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-7 androidusb.pid=0x065d androidkey.dummy=1 androidtouch.htc_event=1 disk_mode_enable=1
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-7 androidusb.pid=0x065d androidkey.dummy=1 androidtouch.htc_event=1 disk_mode_enable=1 androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01f88000 --tags_offset 0x01d88000
@@ -157,16 +157,9 @@ BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
 BOARD_SEPOLICY_UNION += \
     cir_fw_update.te \
     device.te \
-    healthd.te \
     file_contexts \
-    kernel.te \
-    mediaserver.te \
-    qmuxd.te \
-    system_app.te \
-    system_server.te \
-    thermal-engine.te \
-    ueventd.te \
-    vold.te
+    genfs_contexts \
+    system_server.te
 
 # Wifi
 WPA_SUPPLICANT_VERSION      := VER_0_8_X
