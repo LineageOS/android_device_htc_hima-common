200c4791f5164ac83be44fa7c753f674aaefaf27
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-07-06 09:32:05 -0500

    hima: Add handler for HTC touchscreen gestures
    * This is taken from the SlimRoms implementation of DeviceHandler
      and adapted as a service here. We'll control it through framework.
    
    * TODO: Add custom actions to swipe/camera gestures
    
    Change-Id: Ifa5c193ff6501976b33952040c6449a176ae699d

---
+++ gestures/src/com/cyanogenmod/settings/device/HtcGestureService.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2014 SlimRoms Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cyanogenmod.settings.device;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.hardware.SensorEvent;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
+import android.util.Log;
+
+public class HtcGestureService extends Service {
+
+    private static final boolean DEBUG = false;
+
+    public static final String TAG = "GestureService";
+
+    private static final String KEY_SWIPE_UP = "swipe_up_action_key";
+    private static final String KEY_SWIPE_DOWN = "swipe_down_action_key";
+    private static final String KEY_SWIPE_LEFT = "swipe_left_action_key";
+    private static final String KEY_SWIPE_RIGHT = "swipe_right_action_key";
+
+    private static final int ACTION_NONE = 0;
+    private static final int ACTION_CAMERA = 1;
+    private static final int ACTION_TORCH = 2;
+
+    private Context mContext;
+    private GestureMotionSensor mDoubleTapSensor;
+    private GestureMotionSensor mSwipeUpSensor;
+    private GestureMotionSensor mSwipeDownSensor;
+    private GestureMotionSensor mSwipeLeftSensor;
+    private GestureMotionSensor mSwipeRightSensor;
+    private PowerManager mPowerManager;
+
+    private int mSwipeUpAction;
+    private int mSwipeDownAction;
+    private int mSwipeLeftAction;
+    private int mSwipeRightAction;
+
+    private GestureMotionSensor.GestureMotionSensorListener mListener =
+        new GestureMotionSensor.GestureMotionSensorListener() {
+        @Override
+        public void onEvent(int type, SensorEvent event) {
+            switch (type) {
+                case GestureMotionSensor.SENSOR_TYPE_DOUBLE_TAP:
+                    mPowerManager.wakeUp(SystemClock.uptimeMillis());
+                    break;
+                case GestureMotionSensor.SENSOR_TYPE_SWIPE_UP:
+                case GestureMotionSensor.SENSOR_TYPE_SWIPE_DOWN:
+                case GestureMotionSensor.SENSOR_TYPE_SWIPE_LEFT:
+                case GestureMotionSensor.SENSOR_TYPE_SWIPE_RIGHT:
+                case GestureMotionSensor.SENSOR_TYPE_CAMERA:
+                    /* TODO: Handle these */
+                    break;
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        if (DEBUG) Log.d(TAG, "Creating service");
+        super.onCreate();
+
+        mContext = this;
+        mDoubleTapSensor = new GestureMotionSensor(mContext, GestureMotionSensor.SENSOR_TYPE_DOUBLE_TAP);
+        mDoubleTapSensor.registerListener(mListener);
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+        loadPreferences(sharedPrefs);
+        sharedPrefs.registerOnSharedPreferenceChangeListener(mPrefListener);
+        mPowerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        mContext.registerReceiver(mScreenStateReceiver, screenStateFilter);
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (DEBUG) Log.d(TAG, "Destroying service");
+        super.onDestroy();
+        unregisterReceiver(mScreenStateReceiver);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void onDisplayOn() {
+        if (DEBUG) Log.d(TAG, "Display on");
+        if (isDoubleTapEnabled()) {
+            mDoubleTapSensor.disable();
+        }
+    }
+
+    private void onDisplayOff() {
+        if (DEBUG) Log.d(TAG, "Display off");
+        if (isDoubleTapEnabled()) {
+            mDoubleTapSensor.enable();
+        }
+    }
+
+    private boolean isDoubleTapEnabled() {
+        return (Settings.Secure.getInt(mContext.getContentResolver(),
+                    Settings.Secure.DOUBLE_TAP_TO_WAKE, 0) != 0);
+    }
+
+    private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                onDisplayOff();
+            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                onDisplayOn();
+            }
+        }
+    };
+
+    private void loadPreferences(SharedPreferences sharedPreferences) {
+        try {
+            mSwipeUpAction = Integer.parseInt(sharedPreferences.getString(KEY_SWIPE_UP,
+                        Integer.toString(ACTION_NONE)));
+            mSwipeDownAction = Integer.parseInt(sharedPreferences.getString(KEY_SWIPE_DOWN,
+                        Integer.toString(ACTION_NONE)));
+            mSwipeLeftAction = Integer.parseInt(sharedPreferences.getString(KEY_SWIPE_LEFT,
+                    Integer.toString(ACTION_NONE)));
+            mSwipeRightAction = Integer.parseInt(sharedPreferences.getString(KEY_SWIPE_RIGHT,
+                        Integer.toString(ACTION_NONE)));
+        } catch (NumberFormatException e) {
+            Log.e(TAG, "Error loading preferences");
+        }
+    }
+
+    private SharedPreferences.OnSharedPreferenceChangeListener mPrefListener =
+            new SharedPreferences.OnSharedPreferenceChangeListener() {
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            /* TODO: Enable corresponding GestureSensor member variable also */
+            try {
+                if (KEY_SWIPE_UP.equals(key)) {
+                    mSwipeUpAction = Integer.parseInt(sharedPreferences.getString(KEY_SWIPE_UP,
+                                Integer.toString(ACTION_NONE)));
+                } else if (KEY_SWIPE_DOWN.equals(key)) {
+                    mSwipeDownAction = Integer.parseInt(sharedPreferences.getString(KEY_SWIPE_DOWN,
+                                Integer.toString(ACTION_NONE)));
+                } else if (KEY_SWIPE_LEFT.equals(key)) {
+                    mSwipeLeftAction = Integer.parseInt(sharedPreferences.getString(KEY_SWIPE_LEFT,
+                                Integer.toString(ACTION_NONE)));
+                } else if (KEY_SWIPE_RIGHT.equals(key)) {
+                    mSwipeRightAction = Integer.parseInt(sharedPreferences.getString(KEY_SWIPE_RIGHT,
+                                Integer.toString(ACTION_NONE)));
+                }
+            } catch (NumberFormatException e) {
+                Log.e(TAG, "Error loading preferences");
+            }
+        }
+    };
+}
