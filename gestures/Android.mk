977c77502147c2bda07b6c2ddbdb00a06b015e7e
Bruno Martins <bgcngm@gmail.com>
2017-08-21 17:49:44 +0100

    hima-common: GestureHandler: Commonize strings translations
     * Translations moved to the new project
         android_packages_resources_devicesettings
    
    Change-Id: Ib23748771e88bd186e920faf4f985a230b3b0661

--- gestures/Android.mk
+++ gestures/Android.mk
@@ -7,6 +7,12 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v13 \
     org.cyanogenmod.platform.internal
 
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/res \
+    $(LOCAL_PATH)/../../../../packages/resources/devicesettings/res
+
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := GestureHandler
