cffb3c01bf29f6173ffee81238c8f64cc449c82e
Ali B <abittin@gmail.com>
2017-12-02 12:47:57 +0300

    hima-common: move audio config to vendor
    Change-Id: Iccd07b4fad76dce57c2c974d9eeb0a226ebf398e

--- hima.mk
+++ hima.mk
@@ -88,9 +88,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:system/vendor/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/vendor/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/vendor/etc/mixer_paths.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
